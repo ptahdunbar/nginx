74909fdcb3544586fda56cb9dbe42519b52ac020
Ptah Dunbar <yarr@piratedunbar.com>
2013-11-30 18:42:14 -0500

    First pass on fixing nginx for ubuntu precise LTS x64

--- recipes/package.rb
+++ recipes/package.rb
@@ -20,23 +20,7 @@
 
 include_recipe 'nginx::ohai_plugin'
 
-if platform_family?('rhel')
-  if node['nginx']['repo_source'] == 'epel'
-    include_recipe 'yum::epel'
-  elsif node['nginx']['repo_source'] == 'nginx'
-    include_recipe 'nginx::repo'
-  elsif node['nginx']['repo_source'].nil?
-    log "node['nginx']['repo_source'] was not set, no additional yum repositories will be installed." do
-      level :debug
-    end
-  else
-    fail ArgumentError, "Unknown value '#{node['nginx']['repo_source']}' was passed to the nginx cookbook."
-  end
-elsif platform_family?('debian')
-  if node['nginx']['repo_source'] == 'nginx'
-    include_recipe 'nginx::repo'
-  end
-end
+include_recipe 'nginx::repo'
 
 package node['nginx']['package_name'] do
   notifies :reload, 'ohai[reload_nginx]', :immediately
