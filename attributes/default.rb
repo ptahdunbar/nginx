1d06cde2739c1e21bfe1c2c527df6cec93cbd0a8
Pirate Dunbar <yarr@piratedunbar.com>
2013-12-03 13:55:12 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -91,7 +91,7 @@ default['nginx']['access_log_options']     = nil
 default['nginx']['error_log_options']      = nil
 default['nginx']['disable_access_log']     = false
 default['nginx']['install_method']         = 'package'
-default['nginx']['default_site_enabled']   = true
+default['nginx']['default_site_enabled']   = false
 default['nginx']['types_hash_max_size']    = 2_048
 default['nginx']['types_hash_bucket_size'] = 64
 
